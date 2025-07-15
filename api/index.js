24bd1976b3ef8afca887cfc0ca11e783caf2efb4
Daffa Maulana <daffamaulana30@gmail.com>
2025-07-15 17:26:04 +0700

    update

--- api/index.js
+++ api/index.js
@@ -2,37 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("../src/config/dbConfig");
-
-const sensorRoutes = require("../src/routes/sensorRoutes.js");
-const path = require("path");
+const sensorRoutes = require("../src/routes/sensorRoutes");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello from Node API");
-});
-
-connectDB(() => {
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
-});
 
 app.use((req, res, next) => {
   console.log(`Request: ${req.method} ${req.url}`);
 
   if (req.body && Object.keys(req.body).length > 0) {
-    console.log(req.body);
+    console.log("Body:", req.body);
   }
   if (req.query && Object.keys(req.query).length > 0) {
-    console.log("query:", req.query);
+    console.log("Query:", req.query);
   }
   if (req.params && Object.keys(req.params).length > 0) {
-    console.log("params:", req.params);
+    console.log("Params:", req.params);
   }
 
   res.on("finish", () => {
@@ -43,8 +30,21 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello from Node API (Custom Server)");
+});
 
 app.use("/api/sensor", sensorRoutes);
+
+// Hanya untuk lokal, tapi tetap bisa dideploy ke Vercel
+if (process.env.NODE_ENV !== "production") {
+  connectDB(() => {
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  });
+} else {
+  connectDB(); // agar tetap connect saat deploy di Vercel
+}
 
 module.exports = app;
